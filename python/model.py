838a7055d8e120bb5275109b829f77ab52d022d9
h-y-e-j-i <11th_hj@naver.com>
2022-03-09 14:05:52 +0900

    delete Darknet53

--- python/model.py
+++ python/model.py
@@ -102,67 +102,6 @@ def conv_batch(in_num, out_num, kernel_size=3, padding=1, stride=1):
         nn.LeakyReLU(),
         nn.Dropout(0.5))
 
-# Residual block
-class DarkResidualBlock(nn.Module):
-    def __init__(self, in_channels):
-        super(DarkResidualBlock, self).__init__()
-
-        reduced_channels = int(in_channels/2)
-
-        self.layer1 = conv_batch(in_channels, reduced_channels, kernel_size=1, padding=0)
-        self.layer2 = conv_batch(reduced_channels, in_channels)
-
-    def forward(self, x):
-        residual = x
-
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out += residual
-        return out
-
-
-class Darknet53(nn.Module):
-    def __init__(self, block, num_classes):
-        super(Darknet53, self).__init__()
-        self.num_classes = num_classes
-        # block = DarkResidualBlock
-
-        self.features = nn.Sequential(
-            conv_batch(3, 32),
-            conv_batch(32, 64, stride=2),
-            self.make_layer(block, in_channels=64, num_blocks=1),
-            conv_batch(64, 128, stride=2),
-            self.make_layer(block, in_channels=128, num_blocks=2),
-            conv_batch(128, 256, stride=2),
-            self.make_layer(block, in_channels=256, num_blocks=8),
-            conv_batch(256, 512, stride=2),
-            self.make_layer(block, in_channels=512, num_blocks=8),
-            conv_batch(512, 1024, stride=2),
-            self.make_layer(block, in_channels=1024, num_blocks=4),
-        )
-        self.dropout1 = nn.Dropout(0.7)
-        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.classifier = nn.Linear(1024, self.num_classes)
-
-    def forward(self, x):
-        out = self.features(x)
-        out = self.dropout1(out)
-        out = self.global_avg_pool(out)
-        out = out.view(-1, 1024)
-        out = self.classifier(out)
-
-        return out
-
-    def make_layer(self, block, in_channels, num_blocks):
-        layers = []
-        for i in range(0, num_blocks):
-            layers.append(block(in_channels))
-        return nn.Sequential(*layers)
-
-
-def darknet53(num_classes):
-    return Darknet53(DarkResidualBlock, num_classes)
-
 
 class MyEnsemble(nn.Module):
 
